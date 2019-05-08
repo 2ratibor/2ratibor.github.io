d9670933f3e8b17d753878ef660ea133d08e8776
Stepanov Denis <d.stepanov@owox.com>
2019-05-08 18:36:38 +0300

    [tp-00001] default config v1.0

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import './scss/style.scss';
+
+const a = 5;
+let obj = {
+	name: 'WFM',
+	age: 35
+};
+
+let {name: b, age: c} = obj;
+
+console.log("hello, world", a, b, c);
+
+console.log(123);
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+// webpack v4
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+	entry: { main: './src/index.js' },
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: '[name].[chunkhash].js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.m?js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env']
+					}
+				}
+			},
+			{
+				test: /\.scss$/,
+				use:  [  'style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
+			}
+		]
+	},
+	plugins: [
+		new CleanWebpackPlugin(),
+		new MiniCssExtractPlugin({
+			filename: 'style.[contenthash].css',
+		}),
+		new HtmlWebpackPlugin({
+			inject: false,
+			hash: true,
+			template: './src/index.html',
+			filename: 'index.html'
+		}),
+		new WebpackMd5Hash()
+	]
+};
